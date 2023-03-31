d813565e079f7793a53cdf078664a2b7cdf3b0be
Urmi0311 <urmi.kothari@sigmainfo.net>
2023-03-31 18:22:16 +0530

    testimonial

--- wp-content/themes/astra-child/functions.php
+++ wp-content/themes/astra-child/functions.php
@@ -22,199 +22,129 @@ endif;
 add_action( 'wp_enqueue_scripts', 'child_theme_configurator_css', 10 );
 
 // END ENQUEUE PARENT ACTION
-/**
- *	Register Movie Type
- *
- */
 
-add_action( 'init', 'rv_movie_cpt' );
-function rv_movie_cpt() {
 
+
+function create_testimonial_post_type() {
     $labels = array(
-        'name'               => _x( 'Movie', 'post type general name', 'engwp' ),
-        'singular_name'      => _x( 'Movie', 'post type singular name', 'engwp' ),
-        'menu_name'          => _x( 'Movies', 'admin menu', 'engwp' ),
-        'name_admin_bar'     => _x( 'Movie', 'add new on admin bar', 'engwp' ),
-        'add_new'            => _x( 'Add New', 'Movie', 'engwp' ),
-        'add_new_item'       => __( 'Add New Movie', 'engwp' ),
-        'new_item'           => __( 'New Movie', 'engwp' ),
-        'edit_item'          => __( 'Edit Movie', 'engwp' ),
-        'view_item'          => __( 'View Movie', 'engwp' ),
-        'all_items'          => __( 'All Movies', 'engwp' ),
-        'search_items'       => __( 'Search Movies', 'engwp' ),
-        'parent_item_colon'  => __( 'Parent Movie:', 'engwp' ),
-        'not_found'          => __( 'No Movies found.', 'engwp' ),
-        'not_found_in_trash' => __( 'No Movies found in Trash.', 'engwp' )
+        'name' => 'Testimonials',
+        'singular_name' => 'Testimonial',
+        'add_new' => 'Add New',
+        'add_new_item' => 'Add New Testimonial',
+        'edit_item' => 'Edit Testimonial',
+        'new_item' => 'New Testimonial',
+        'view_item' => 'View Testimonial',
+        'search_items' => 'Search Testimonials',
+        'not_found' =>  'No Testimonials found',
+        'not_found_in_trash' => 'No Testimonials in the trash',
+        'parent_item_colon' => '',
     );
 
     $args = array(
-            'description'	      => __( 'Movie', 'engwp' ),
-            'labels'	      => $labels,
-            'supports'	      => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'trackbacks', 'custom-fields', 'comments', 'revisions' ),
-            'hierarchical'	      	=> false,
-            'public'	      	=> true,
-            'publicly_queryable'  	=> true,
-            'query_var'	      	=> true,
-            'rewrite'	      	=> array( 'slug' => 'filmer' ),
-    'show_ui'	      	=> true,
-        'menu_icon'	      	=> 'dashicons-format-video',
-        'show_in_menu'	      	=> true,
-        'show_in_nav_menus'	=> true,
-        'show_in_admin_bar'	=> true,
-        // 'menu_position'	=> 5,
-        'can_export'		=> true,
-        'has_archive'		=> true,
-        'exclude_from_search'	=> false,
-        'capability_type'	=> 'post',
+        'labels' => $labels,
+        'public' => true,
+        'publicly_queryable' => true,
+        'show_ui' => true,
+        'exclude_from_search' => true,
+        'query_var' => true,
+        'rewrite' => true,
+        'capability_type' => 'post',
+        'has_archive' => true,
+        'hierarchical' => false,
+        'menu_position' => 20,
+        'supports' => array( 'title', 'editor', 'thumbnail' ),
     );
 
-    register_post_type( 'movie', $args );
+    register_post_type( 'testimonial', $args );
+}
+add_action( 'init', 'create_testimonial_post_type' );
 
+function testimonials_meta_boxes() {
+    add_meta_box( 'testimonials_details', 'Testimonial Details', 'testimonials_details_callback', 'testimonials', 'normal', 'high' );
 }
 
-add_action( 'init', 'rv_create_movie_taxonomies' );
-function rv_create_movie_taxonomies() {
+function testimonials_details_callback( $post ) {
+    wp_nonce_field( 'testimonials_save_meta', 'testimonials_meta_nonce' );
 
-    // Add new taxonomy, make it non-hierarchical (like tags)
-    $labels = array(
-        'name'                       => _x( 'Year Made', 'taxonomy general name' ),
-        'singular_name'              => _x( 'Year', 'taxonomy singular name' ),
-        'search_items'               => __( 'Search Years' ),
-        'all_items'                  => __( 'All Years' ),
-        'parent_item'                => __( 'Parent Year' ),
-        'parent_item_colon'          => __( 'Parent Year:' ),
-        'edit_item'                  => __( 'Edit Year' ),
-        'update_item'                => __( 'Update Year' ),
-        'add_new_item'               => __( 'Add New Year' ),
-        'new_item_name'              => __( 'New Year Name' ),
-        'separate_items_with_commas' => __( 'Separate Years with commas' ),
-        'add_or_remove_items'        => __( 'Add or remove Years' ),
-        'choose_from_most_used'      => __( 'Choose from the most used Years' ),
-        'not_found'                  => __( 'No Years found.' ),
-        'menu_name'                  => __( 'Year Made' ),
-    );
+    $testimonial_name = get_post_meta( $post->ID, '_testimonial_name', true );
+    $testimonial_company = get_post_meta( $post->ID, '_testimonial_company', true );
+    $testimonial_url = get_post_meta( $post->ID, '_testimonial_url', true );
+    ?>
 
-    $args = array(
-        'hierarchical'          => false,
-        'labels'                => $labels,
-        'show_ui'               => true,
-        'show_admin_column'     => true,
-        'update_count_callback' => '_update_post_term_count',
-        // 'query_var'          => true,
-        // 'show_in_nav_menus'	=> false,
-        'public'		=> true,
-        'publicly_queryable'	=> true,
-        'has_archive'		=> true,
-    );
+    <p>
+        <label for="testimonial_name">Name:</label>
+        <input type="text" name="testimonial_name" id="testimonial_name" value="<?php echo esc_attr( $testimonial_name ); ?>" />
+    </p>
 
-    $years = array( 'rewrite' => array( 'slug' => 'movie-year' ) );
+    <p>
+        <label for="testimonial_company">Company:</label>
+        <input type="text" name="testimonial_company" id="testimonial_company" value="<?php echo esc_attr( $testimonial_company ); ?>" />
+    </p>
 
-    $movie_args = array_merge( $args, $years );
+    <p>
+        <label for="testimonial_url">URL:</label>
+        <input type="text" name="testimonial_url" id="testimonial_url" value="<?php echo esc_url( $testimonial_url ); ?>" />
+    </p>
 
-    register_taxonomy( 'movie_years', 'movie', $movie_args );
-
+    <?php
 }
 
-//* Add post type support within the Genesis Framework
-if ( 'genesis' == basename( TEMPLATEPATH ) )
-    add_post_type_support( 'movie', 'genesis-cpt-archives-settings' );
-
-
-
-//* Create Moive Type custom taxonomy (category)
-add_action( 'init', 'custom_type_taxonomy' );
-function custom_type_taxonomy() {
-
-    register_taxonomy( 'movie-type', 'movie',
-        array(
-            'labels' => array(
-                'name'          => _x( 'Movie Category', 'taxonomy general name', 'text_domain' ),
-                'add_new_item'  => __( 'Add New Movie Category', 'text_domain' ),
-                'new_item_name' => __( 'New Movie Type', 'text_domain' ),
-            ),
-            'exclude_from_search' => true,
-            'has_archive'         => true,
-            'hierarchical'        => true,
-            'rewrite'             => array( 'slug' => 'movie-type', 'with_front' => false ),
-            'show_ui'             => true,
-            'show_tagcloud'       => false,
-        )
-    );
-
-}
-
-//function to generate shortcode to display the posts present in  movie
-function custom_taxonomy_shortcode( $atts ) {
-    // Extract shortcode attributes
-    extract( shortcode_atts( array(
-        'taxonomy' => 'movie-type',
-        'post_type' => 'movie',
-    ), $atts ) );
-
-    // Check if a specific term has been selected
-    if ( isset( $_GET['term'] ) ) {
-        // Retrieve the selected term
-        $selected_term = get_term_by( 'slug', $_GET['term'], $taxonomy );
-
-        // Build a WP_Query to retrieve all movies assigned to the selected term
-        $query_args = array(
-            'post_type' => $post_type,
-            'tax_query' => array(
-                array(
-                    'taxonomy' => $taxonomy,
-                    'field' => 'slug',
-                    'terms' => $_GET['term'],
-                ),
-            ),
-        );
-        $movies_query = new WP_Query( $query_args );
-
-        // Output the list of movies assigned to the selected term
-        $output = '<h2>' . esc_html( $selected_term->name ) . '</h2>';
-        $output .= '<ul>';
-
-        while ( $movies_query->have_posts() ) {
-            $movies_query->the_post();
-
-            // Build the link to the movie
-            $link = get_permalink();
-            $output .= '<li>';
-            $output .= '<a href="' . esc_url( $link ) . '">' . get_the_title() . '</a>';
-            $output .= '<div>' . get_the_post_thumbnail() . '</div>';
-            $output .= '<div>' . get_the_excerpt() . '</div>';
-            $output .= '</li>';
-        }
-
-        $output .= '</ul>';
-
-        // Reset the post data
-        wp_reset_postdata();
-    } else {
-        // Retrieve all terms in the specified taxonomy
-        $terms = get_terms( array(
-            'taxonomy' => $taxonomy,
-            'hide_empty' => false,
-        ) );
-
-        if ( ! empty( $terms ) && ! is_wp_error( $terms ) ) {
-            // Output the list of terms
-            $output = '<h2>Select ' . esc_html( $taxonomy ) . ':</h2>';
-            $output .= '<ul>';
-
-            foreach ( $terms as $term ) {
-                // Build the link to the term
-                $link = add_query_arg( 'term', $term->slug );
-                $output .= '<li><h3><a href="' . esc_url( $link ) . '">' . esc_html( $term->name ) . '</a></h3></li>';
-            }
-
-            $output .= '</ul>';
-        } else {
-            // Output a message if no terms are found
-            $output = '<p>No ' . esc_html( $taxonomy ) . ' found.</p>';
-        }
+function testimonials_save_meta( $post_id ) {
+    if ( ! isset( $_POST['testimonials_meta_nonce'] ) || ! wp_verify_nonce( $_POST['testimonials_meta_nonce'], 'testimonials_save_meta' ) ) {
+        return;
     }
+
+    if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+        return;
+    }
+
+    if ( ! current_user_can( 'edit_post', $post_id ) ) {
+        return;
+    }
+
+    $testimonial_name = isset( $_POST['testimonial_name'] ) ? sanitize_text_field( $_POST['testimonial_name'] ) : '';
+    $testimonial_company = isset( $_POST['testimonial_company'] ) ? sanitize_text_field( $_POST['testimonial_company'] ) : '';
+    $testimonial_url = isset( $_POST['testimonial_url'] ) ? esc_url_raw( $_POST['testimonial_url'] ) : '';
+
+    update_post_meta( $post_id, '_testimonial_name', $testimonial_name );
+    update_post_meta( $post_id, '_testimonial_company', $testimonial_company );
+    update_post_meta( $post_id, '_testimonial_url', $testimonial_url );
+}
+
+add_action( 'add_meta_boxes', 'testimonials_meta_boxes' );
+add_action( 'save_post', 'testimonials_save_meta' );
+
+
+function testimonials_slider_shortcode() {
+    $query = new WP_Query( array(
+        'post_type' => 'testimonial',
+        'posts_per_page' => -1,
+    ) );
+
+    $output = '<div class="testimonial-slider">';
+
+    while ( $query->have_posts() ) {
+        $query->the_post();
+        $output .= '<div class="testimonial-slide">';
+        $output .= '<blockquote>';
+        $output .= get_the_content();
+        $output .= '<cite>' . get_the_title() . '</cite>';
+        $output .= '</blockquote>';
+        $output .= '</div>';
+    }
+
+    $output .= '</div>';
+
+    wp_reset_postdata();
 
     return $output;
 }
+add_shortcode( 'testimonials-slider', 'testimonials_slider_shortcode' );
 
-add_shortcode( 'custom-taxonomy', 'custom_taxonomy_shortcode' );
+
+// Enqueue testimonial slider script
+function enqueue_testimonial_slider_script() {
+    wp_enqueue_script( 'testimonial-slider', get_stylesheet_directory_uri() . '/js/testimonial-slider.js', array('jquery'), '1.0', true );
+}
+add_action( 'wp_enqueue_scripts', 'enqueue_testimonial_slider_script' );
+
--- wp-content/themes/astra/functions.php
+++ wp-content/themes/astra/functions.php
@@ -193,3 +193,69 @@ function social_button_shortcode( $atts ) {
     }
 }
 add_shortcode( 'social_button', 'social_button_shortcode' );
+
+add_action('init', 'custom_post_type_movies');
+function custom_post_type_movies() {
+    $supports = array(
+        'title', // post title
+        'editor', // post content
+        'author', // post author
+        'thumbnail', // featured images
+        'excerpt', // post excerpt
+        'custom-fields', // custom fields
+        'comments', // post comments
+        'revisions', // post revisions
+        'post-formats', // post formats
+    );
+
+    $labels = array(
+        'name' => _x('Movie', 'plural'),
+        'singular_name' => _x('Movie', 'singular'),
+        'menu_name' => _x('Movie', 'admin menu'),
+        'name_admin_bar' => _x('Movie', 'admin bar'),
+        'add_new' => _x('Add New', 'add new'),
+        'add_new_item' => __('Add New Movie'),
+        'new_item' => __('New Movie'),
+        'edit_item' => __('Edit Movie'),
+        'view_item' => __('View Movie'),
+        'all_items' => __('All Movie'),
+        'search_items' => __('Search Movie'),
+        'not_found' => __('No Movie found.'),
+    );
+
+    $args = array(
+        'supports' => $supports,
+        'labels' => $labels,
+        'description' => 'Holds our movie and specific data',
+        'public' => true,
+        'taxonomies' => array( 'category'),
+        'show_ui' => true,
+        'show_in_menu' => true,
+        'show_in_nav_menus' => true,
+        'show_in_admin_bar' => true,
+        'can_export' => true,
+        'capability_type' => 'post',
+        'show_in_rest' => true,
+        'query_var' => true,
+        'rewrite' => array('slug' => 'movie'),
+        'has_archive' => true,
+        'hierarchical' => true,
+        'menu_position' => 6,
+        'menu_icon' => 'dashicons-megaphone',
+    );
+
+    register_post_type('movie', $args); // Register Post type
+}
+
+add_filter('pre_get_posts', 'query_post_type');
+function query_post_type($query) {
+    if( is_category() ) {
+        $post_type = get_query_var('post_type');
+        if($post_type)
+            $post_type = $post_type;
+        else
+            $post_type = array('nav_menu_item', 'post', 'movie'); // don't forget nav_menu_item to allow menus to work!
+        $query->set('post_type',$post_type);
+        return $query;
+    }
+}
